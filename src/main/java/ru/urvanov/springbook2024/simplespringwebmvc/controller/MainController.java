a91f4621e2c35387f39c81e75529b18881b37abf
Fedor Urvanov <fedor@urvanov.ru>
2024-01-28 15:27:09 +0300

    Main controller first commit

---
+++ src/main/java/ru/urvanov/springbook2024/simplespringwebmvc/controller/MainController.java
@@ -0,0 +1,15 @@
+package ru.urvanov.springbook2024.simplespringwebmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "Hello, world!";
+    }
+
+}
