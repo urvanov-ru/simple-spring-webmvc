978f73453c07aaa9af9b4766a778c722243c34f7
Fedor Urvanov <fedor@urvanov.ru>
2024-01-28 15:31:42 +0300

    organize imports

--- src/main/java/ru/urvanov/springbook2024/simplespringwebmvc/controller/MainController.java
+++ src/main/java/ru/urvanov/springbook2024/simplespringwebmvc/controller/MainController.java
@@ -1,6 +1,5 @@
 package ru.urvanov.springbook2024.simplespringwebmvc.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
